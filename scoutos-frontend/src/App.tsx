69e47ef120ae5ac732fe77d2801ba215a509dcb7
asr319 <80777760+asr319@users.noreply.github.com>
2025-07-12 18:19:10 -0500

    Fix Node.js workflow (#146)
    Fix node.js workflow

--- scoutos-frontend/src/App.tsx
+++ scoutos-frontend/src/App.tsx
@@ -3,33 +3,13 @@ import MemoryManager from './components/MemoryManager';
 import AuthForm from './components/AuthForm';
 import { Toaster } from 'react-hot-toast';
 import { UserProvider, useUser } from './context/UserContext';
-import MemoryManager from './components/MemoryManager';
-import AuthForm from './components/AuthForm';
-import { Toaster } from 'react-hot-toast';
-import { UserProvider } from './context/UserContext';
-import { useUser } from './hooks/useUser';
 import './index.css';
-
-function Main() {
-  const { user } = useUser();
-  return user ? (
-    <div className="space-y-4">
-      <ChatInterface />
-      <MemoryManager />
-    </div>
-  ) : (
-    <AuthForm />
-  );
-}
 
 function App() {
   return (
-    <UserProvider>
-      <div className="min-h-screen bg-gray-100 dark:bg-gray-900 p-4">
-        <Toaster position="top-right" />
-        <Main />
-      </div>
-    </UserProvider>
+    <div className="min-h-screen bg-gray-100">
+      <ChatInterface />
+    </div>
   );
 }
 
