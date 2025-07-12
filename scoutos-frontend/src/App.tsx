88d22f30411c9024291d71637cfd18e20629a4a4
ar0512 <155201642+ar0512@users.noreply.github.com>
2025-07-12 16:43:44 -0500

    merge

--- scoutos-frontend/src/App.tsx
+++ scoutos-frontend/src/App.tsx
@@ -1,11 +1,31 @@
 import ChatInterface from './components/ChatInterface';
+import MemoryManager from './components/MemoryManager';
+import AuthForm from './components/AuthForm';
+import { Toaster } from 'react-hot-toast';
+import { UserProvider } from './context/UserContext';
+import { useUser } from './hooks/useUser';
 import './index.css';
+
+function Main() {
+  const { user } = useUser();
+  return user ? (
+    <div className="space-y-4">
+      <ChatInterface />
+      <MemoryManager />
+    </div>
+  ) : (
+    <AuthForm />
+  );
+}
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-100">
-      <ChatInterface />
-    </div>
+    <UserProvider>
+      <div className="min-h-screen bg-gray-100 dark:bg-gray-900 p-4">
+        <Toaster position="top-right" />
+        <Main />
+      </div>
+    </UserProvider>
   );
 }
 
