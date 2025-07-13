02e2ff8e2caff71ccaa85c13c075710d13a89321
asr319 <80777760+asr319@users.noreply.github.com>
2025-07-13 00:53:36 -0500

    feat(frontend): add toast notifications and loading

--- scoutos-frontend/src/App.tsx
+++ scoutos-frontend/src/App.tsx
@@ -2,7 +2,6 @@ import ChatInterface from './components/ChatInterface';
 import AuthForm from './components/AuthForm';
 import { useUser } from './hooks/useUser';
 import { Toaster } from 'react-hot-toast';
-import { UserProvider } from '../context/UserContext';
 import './index.css';
 
 function AppContent() {
